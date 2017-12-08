52981202162cbb6424723d6ba52e552dcc38b80b
cross.william <cross.william@306A-16.charterschool.local>
2017-12-08 14:12:35 -0500

    the main does something

--- src/c_box/C_box.java
+++ src/c_box/C_box.java
@@ -15,7 +15,7 @@ public class C_box {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        cBox c = new cBox();
     }
     
 }
